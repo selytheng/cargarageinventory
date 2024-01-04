e8eaf77dfb50df06059b737d2e462a7c6a1162ab
selytheng <kh.lytheng@gmail.com>
2024-01-04 16:16:20 +0700

    Initial commit

---
+++ src/test/java/carinventory/itc/cargarageinventory/CargarageinventoryApplicationTests.java
@@ -0,0 +1,13 @@
+package carinventory.itc.cargarageinventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CargarageinventoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
