38e2d74b3e94f588c6aa95db6116ac23f6180ccc
Seavmuy <pinseavmouy99@gmail.com>
2024-01-08 15:27:50 +0700

    First commit

---
+++ .history/src/main/java/carinventory/itc/cargarageinventory/CargarageinventoryApplication_20240106225731.java
@@ -0,0 +1,15 @@
+package carinventory.itc.cargarageinventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CargarageinventoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CargarageinventoryApplication.class, args);
+	
+	
+	}
+
+}
