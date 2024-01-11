ff704a16bd83fa2d358891811ab3a8b461d8c4a6
selytheng <kh.lytheng@gmail.com>
2024-01-11 14:43:57 +0700

    change html front-end

--- src/main/java/carinventory/itc/cargarageinventory/repository/VehicleRepository.java
+++ src/main/java/carinventory/itc/cargarageinventory/repository/VehicleRepository.java
@@ -11,14 +11,4 @@ public interface VehicleRepository extends JpaRepository<Vehicle, Integer>, JpaS
 
     Optional<Vehicle> findById(int id);
 
-    // @Query("SELECT v FROM Vehicle v WHERE (:modelYear IS NULL OR v.modelYear =
-    // :modelYear) OR (:manufacturer IS NULL OR v.manufacturer = :manufacturer) AND
-    // (:model IS NULL OR v.model = :model) AND (:color IS NULL OR v.color =
-    // :color)")
-    // List<Vehicle> searchVehicles(
-    // @Param("modelYear") Integer modelYear,
-    // @Param("manufacturer") String manufacturer,
-    // @Param("model") String model,
-    // @Param("color") String color);
-
 }
